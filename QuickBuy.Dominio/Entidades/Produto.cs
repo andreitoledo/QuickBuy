d8944000d4b919c6a90acb4856bec49ff4907d8b
andreitoledo <andreiltoledo@hotmail.com>
2023-01-15 10:37:49 -0300

    ajuste em produtos

--- QuickBuy.Dominio/Entidades/Produto.cs
+++ QuickBuy.Dominio/Entidades/Produto.cs
@@ -5,9 +5,7 @@
         public int Id { get; set; }
         public string Nome { get; set; }
         public string Descricao { get; set; }
-        public decimal Preco { get; set; }
-
-        // teste produto
+        public decimal Preco { get; set; }       
 
         public override void Validate()
         {
