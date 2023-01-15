e2ecf7db858a4844bbc2cf7b83f8e72f7c6ccd66
andreitoledo <andreiltoledo@hotmail.com>
2023-01-15 09:19:50 -0300

    ajustando commit

---
+++ QuickBuy.Repositorio/Repositorios/PedidoRepositorio.cs
@@ -0,0 +1,14 @@
+﻿using QuickBuy.Dominio.Contratos;
+using QuickBuy.Dominio.Entidades;
+using QuickBuy.Repositorio.Contexto;
+
+namespace QuickBuy.Repositorio.Repositorios
+{
+    public class PedidoRepositorio : BaseRepositorio<Pedido>, IPedidoRepositorio
+    {
+        //teste
+        public PedidoRepositorio(QuickBuyContexto quickBuyContexto) : base(quickBuyContexto)
+        {
+        }
+    }
+}
