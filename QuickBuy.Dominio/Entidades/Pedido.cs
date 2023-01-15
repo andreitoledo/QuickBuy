865ef5286a1ca728b079e0cb3adcc0c98b8b9a25
andreitoledo <andreiltoledo@hotmail.com>
2023-01-15 09:24:43 -0300

    inserindo comentario

--- QuickBuy.Dominio/Entidades/Pedido.cs
+++ QuickBuy.Dominio/Entidades/Pedido.cs
@@ -18,7 +18,9 @@ namespace QuickBuy.Dominio.Entidades
         public string EnderecoCompleto { get; set; }
         public int NumeroEndereco { get; set; }
         public int FormaPagamentoId { get; set; }
-        public virtual FormaPagamento FormaPagamento { get; set; }           
+        public virtual FormaPagamento FormaPagamento { get; set; }      
+        
+        //teste
         
    
         /// <summary>
