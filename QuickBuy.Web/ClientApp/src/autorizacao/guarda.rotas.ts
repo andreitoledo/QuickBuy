cb838bfbf1f5a3c2582379ecd1dd95f1559dfdaf
andreitoledo <andreiltoledo@hotmail.com>
2023-01-18 11:49:25 -0300

    Correção no decorator da classe GuargaRotas

--- QuickBuy.Web/ClientApp/src/autorizacao/guarda.rotas.ts
+++ QuickBuy.Web/ClientApp/src/autorizacao/guarda.rotas.ts
@@ -2,7 +2,7 @@ import { Injectable } from "@angular/core";
 import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from "@angular/router";
 
 
-Injectable({
+@Injectable({
   providedIn:'root'
 
 })
